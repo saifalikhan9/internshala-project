81b3a17d06d46d135091d285a3a815bd82b8386f
Saifalikhan99 <myname14saif@gmail.com>
2025-06-04 12:16:02 +0530

    Update padding in PhotographerProfile component for improved layout

--- components/photographers/profile.tsx
+++ components/photographers/profile.tsx
@@ -31,7 +31,7 @@
 
 
     return (
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-4 py-20  ">
         <div className="flex items-center mb-6">
           <Button variant="ghost" size="sm" onClick={() => router.back()}>
             <ArrowLeft className="h-4 w-4 mr-2" />
